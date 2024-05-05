c2b96c29beb4a368455e6f6165a653107ece2300
lidiyazakharova <lidijaromanowa@gmail.com>
2024-05-05 12:40:37 +0300

    edit myNFT

--- FakeNFT/Scenes /Profile/MyNFTPresenter.swift
+++ FakeNFT/Scenes /Profile/MyNFTPresenter.swift
@@ -4,12 +4,19 @@ protocol MyNFTVPresenterProtocol {
     
     var view: MyNFTViewControllerProtocol? { get set }
     
+    
     func loadNfts()
 }
 
 final class MyNFTPresenter: MyNFTVPresenterProtocol {
     
     weak var view: MyNFTViewControllerProtocol?
+    
+//    private let profileService: ProfileService
+//    private let nftService: NftService
+    
+    private var nfts: [Nft] = []
+
     
     func loadNfts() {
         view?.refreshNfts(nfts: mockMyNfts)
