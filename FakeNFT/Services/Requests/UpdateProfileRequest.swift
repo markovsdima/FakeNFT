eee78b570c61beaecd715431d0494e7ea8ce6845
lidiyazakharova <lidijaromanowa@gmail.com>
2024-05-16 11:26:54 +0300

    fix like bug

--- FakeNFT/Services/Requests/UpdateProfileRequest.swift
+++ FakeNFT/Services/Requests/UpdateProfileRequest.swift
@@ -11,15 +11,25 @@ struct UpdateProfileRequest: NetworkRequest {
     var body: Data?
 
     init(profile: ProfileUpdate) {
+        let likesItem: [URLQueryItem]
+        
+        if profile.likes.isEmpty {
+            likesItem = [
+                URLQueryItem(name: "likes", value: "null")
+            ]
+        } else {
+            likesItem = profile.likes.map {
+                URLQueryItem(name: "likes", value: $0)
+            }
+        }
+        
         var components = URLComponents()
         components.queryItems = [
             URLQueryItem(name: "name", value: profile.name),
             URLQueryItem(name: "avatar", value: profile.avatar),
             URLQueryItem(name: "description", value: profile.description),
             URLQueryItem(name: "website", value: profile.website)
-        ] + profile.likes.map {
-            URLQueryItem(name: "likes", value: $0)
-        }
+        ] + likesItem
 
         if let queryString = components.percentEncodedQuery {
             self.body = queryString.data(using: .utf8)
